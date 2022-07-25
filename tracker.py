a4142327e15b4bd1bf90d89c0af2c24a26672bdb
Aaron McKain <maaron41602@gmail.com>
2022-07-24 19:44:51 -0500

    Finally finds the room values

--- tracker.py
+++ tracker.py
@@ -1,3 +1,4 @@
+from webbrowser import get
 from openpyxl.utils import get_column_letter
 from datetime import date, datetime, timedelta
 from openpyxl import Workbook, load_workbook
@@ -33,21 +34,22 @@ def calculate_sum(num_of_times):
 
 
 #stores the cell letters in a list
-for row in range(1,2): #rows 1-29
+for row in range(1,2): #rows 1-2
     for col in range(1, 46): #col 1-40
         room_locations.append(get_column_letter(col))
         print("\nStoring column " + get_column_letter(col) + " in the list room_locations")
         print("The list currently contains the following letters: ")
         for i in room_locations:
             print(i, end = " ")
-        #print("\n") 
-        print("\n")
+        print("\n") 
 
-
+for cell in room_locations:
+    room = ws[cell + str(1)]
+    print(room.value)
 
 #Cycles through each cell
 for cell in room_locations:
-    print("We are currently in cell " + cell) 
+    #print("We are currently in cell " + cell) <><><><><><<><><><><><><><><><><><><><><><><><><><><><><><><><<
     #temporary list that store the times from each room; clears after use
     list_of_times = []
     for i in range(2,39):
@@ -58,13 +60,17 @@ for cell in room_locations:
         
     #Makes the temporary list sorted from earliest to latest      
     list_of_times.reverse()
-    for i in list_of_times:
-        print(i)
+   #for i in list_of_times: <><><><><><<><><><><><><><><><><><><><><><><><><><><><><><><><<
+        #print(i) <><><><><><<><><><><><><><><><><><><><><><><><><><><><><><><><<
     
     
     #Calls calculate average
     average_change = calculate_sum(list_of_times)
     print("The average days between changes is " + str(average_change))
+
+    for i in room_and_averages:
+        room_and_averages[i] = average_change
+        #print(room_and_averages[i])
 
     #clears the list for the next room
     list_of_times.clear()
